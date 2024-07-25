955a49429a7325e1d2cca913302249713598405e
pwuersch <49908921+pwuersch@users.noreply.github.com>
2024-07-25 18:52:48 +0200

    feat: remove bashio config

--- FiatAddon/run.sh
+++ FiatAddon/run.sh
@@ -1,25 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bash
 
-export FiatUconnect_MqttUser=$(bashio::services "mqtt" "username")
-export FiatUconnect_MqttPw=$(bashio::services "mqtt" "password")
-export FiatUconnect_MqttServer=$(bashio::services "mqtt" "host")
-export FiatUconnect_MqttPort=$(bashio::services "mqtt" "port")
-  
-export FiatUconnect_StartDelaySeconds=$(bashio::config 'StartDelaySeconds')
-export FiatUconnect_SupervisorToken=$SUPERVISOR_TOKEN
-  
-export FiatUconnect_FiatUser=$(bashio::config 'FiatUser')
-export FiatUconnect_FiatPw=$(bashio::config 'FiatPw')
-export FiatUconnect_FiatPin=$(bashio::config 'FiatPin')
-
-export FiatUconnect_Brand=$(bashio::config 'Brand')
-export FiatUconnect_Region=$(bashio::config 'Region')
-
-export FiatUconnect_Debug=$(bashio::config 'Debug')
-export FiatUconnect_RefreshInterval=$(bashio::config 'RefreshInterval')
-export FiatUconnect_AutoDeepRefresh=$(bashio::config 'AutoDeepRefresh')
-export FiatUconnect_AutoDeepInterval=$(bashio::config 'AutoDeepInterval')
-export FiatUconnect_StartDelaySeconds=$(bashio::config 'StartDelaySeconds')
-
-cd /build/
+cd /build/ || exit
 ./FiatUconnect
